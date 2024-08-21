8be932e419129b8d7163f5a89e0a6126ca753c11
wlals7565 <117993640+wlals7565@users.noreply.github.com>
2024-08-21 09:28:33 +0900

    [UPDATE]: 7chapter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import IterationSample from "./6장/IterationSample1";
+import LifeCycleSample from "./7장/LifeCycleSample";
 import "./App.css";
 import { Component } from "react";
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <>
-        <IterationSample>
-
-        </IterationSample>
-      </>
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
     );
   }
 }
